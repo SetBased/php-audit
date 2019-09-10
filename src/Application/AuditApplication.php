fb28b78c94b61be8a3743dea1e2696092fe8cbee
Paul Water <p.r.water@setbased.nl>
2019-09-10 15:07:21 +0200

    Added build target version.

--- src/Application/AuditApplication.php
+++ src/Application/AuditApplication.php
@@ -21,7 +21,7 @@ class AuditApplication extends Application
    */
   public function __construct()
   {
-    parent::__construct('AuditApplication');
+    parent::__construct('audit', '0.0.0');
   }
 
   //--------------------------------------------------------------------------------------------------------------------
