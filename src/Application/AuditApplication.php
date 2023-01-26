83c25991ebf1507ec458aa0f469167168b17d281
Paul Water <p.r.water@setbased.nl>
2023-01-26 15:45:22 +0100

    Release: 1.9.0

--- src/Application/AuditApplication.php
+++ src/Application/AuditApplication.php
@@ -21,7 +21,7 @@ class AuditApplication extends Application
    */
   public function __construct()
   {
-    parent::__construct('audit', '1.8.1');
+    parent::__construct('audit', '1.9.0');
   }
 
   //--------------------------------------------------------------------------------------------------------------------
